40789a6d5e88788dc35bce6b588591e06a14a3d8
rafal <ravmiecznk@gmail.com>
2020-01-25 22:37:19 +0100

    resotore module avr_message_sender

---
+++ avr_message_sender_bootloader_/avr_txmessage_sender.cpp
@@ -0,0 +1,106 @@
+/*
+ * avr_message_sender.cpp
+ *
+ *  Created on: 11 wrz 2019
+ *      Author: rafal
+ */
+
+
+#include "avr_txmessage_sender.h"
+#include <avr/crc16.h>
+#include <string.h>
+#include "../uart/uart_standard.h"
+
+using namespace tx_id;
+
+
+//***CONSTRUCTORS***
+TxMessage::TxMessage(uint16_t msg_id, uint16_t context){
+	tail.id = msg_id;
+	tail.context = context;
+	//_stdout.put	= &PUTCHAR;
+}
+
+
+TxMessage::TxMessage(){
+	tail.id = tx_id::txt;
+	tail.context = 0;
+	//_stdout.put	= &PUTCHAR;
+}
+
+TxMessage::~TxMessage(){
+	send_tail();
+}
+
+//***METHODS***
+void TxMessage::_putchar(char c){
+	uart1_putc(c);
+}
+
+//int Message::_putchar_f(char c, FILE* f){
+//	usart.Putchar(c);
+//	return 1;
+//}
+
+void TxMessage::send(char* buffer, uint16_t amount){
+	for(uint16_t i=0; i<amount; i++){
+		tail.msg_len++;
+		tail.msg_crc = _crc_xmodem_update(tail.msg_crc, *buffer);
+		uart1_putc(*buffer);
+		buffer++;				//pointer increment
+	}
+	send_tail();
+}
+
+void TxMessage::send(char c){
+	tail.msg_len++;
+	tail.msg_crc = _crc_xmodem_update(tail.msg_crc, c);
+	uart1_putc(c);
+	send_tail();
+}
+
+
+void TxMessage::fetch_str(char* buffer){
+	while(*buffer){
+		tail.msg_len++;
+		tail.msg_crc = _crc_xmodem_update(tail.msg_crc, *buffer);
+		uart1_putc(*buffer);
+		buffer++;				//pointer increment
+	}
+}
+
+void TxMessage::sends(char* buffer){
+	fetch_str(buffer);
+	send_tail();
+}
+
+
+void TxMessage::fetch_byte(uint8_t c){
+	uart1_putc(c);
+	tail.msg_len++;
+	tail.msg_crc = _crc_xmodem_update(tail.msg_crc, c);
+}
+
+void TxMessage::send_tail(){
+	/*
+	 * Method to close the message by sending its tail
+	 */
+	uint16_t tail_crc = 0;
+	uint8_t* tail_crc_p = (uint8_t*)&tail_crc;
+	char c;
+	if(not tail_was_sent){
+		tail_was_sent = true;
+		uint8_t* p_tail = (uint8_t*)tail;
+		for(uint8_t i=0; i<tail_size; i++){
+			c = *p_tail++;
+			_putchar(c);
+			tail_crc = _crc_xmodem_update(tail_crc, c);
+		}
+		for(uint8_t i=0; i<2; i++){
+			_putchar(tail_crc_p[i]);
+		}
+	}
+}
+
+uint8_t TxMessage::tail_size = sizeof(Tail);
+
---
+++ avr_message_sender_bootloader_/avr_txmessage_sender.h
@@ -0,0 +1,75 @@
+/*
+ * avr_message_sender.h
+ *
+ *  Created on: 11 wrz 2019
+ *      Author: rafal
+ */
+
+#ifndef AVR_MESSAGE_SENDER_AVR_TXMESSAGE_SENDER_H_
+#define AVR_MESSAGE_SENDER_AVR_TXMESSAGE_SENDER_H_
+
+#include <stdint.h>
+#include "../setup.h"
+
+#define TAIL_START_MARK	'<'
+#define TAIL_END_MARK	'>'
+
+
+
+
+namespace tx_id {
+	enum id{
+		ack_feedback,
+		nak_feedback,
+		dtx,
+		txt,
+		dbg,
+		digidiag_frame,
+		pin_change_pending,
+	};
+}
+
+struct Tail{
+	uint8_t 	tail_start=	TAIL_START_MARK;	//'>'
+	uint16_t	id=			0;
+	uint16_t	context=	0;
+	uint16_t 	msg_len=	0;
+	uint16_t	msg_crc=	0;
+	uint8_t		tail_end=	TAIL_END_MARK;	//'<'
+
+	operator uint8_t* (){
+		return &tail_start;
+	}
+};
+
+class TxMessage{
+	/*
+	 * Class to create and send message.
+	 * |MESSAGE BODY|<TAIL>crc_tail
+	 *
+	 * When the object is destroyed the tail is sent
+	 * which closes the message integrity
+	 *
+	 */
+private:
+	void 			_putchar(char);
+	static uint8_t 	tail_size;
+	Tail 			tail;
+	bool			tail_was_sent = false;
+public:
+
+	TxMessage(uint16_t msg_id, uint16_t context=0);
+	TxMessage();
+	~TxMessage();
+	void 	send(char*, uint16_t);
+	void 	send(char c);
+	void 	sends(char*);
+	void	fetch_str(char*);
+	void 	fetch_byte(uint8_t c);
+	void	send_tail();
+};
+
+
+
+
+#endif /* AVR_MESSAGE_SENDER_AVR_TXMESSAGE_SENDER_H_ */
